dfa6e41134c6a53f1afa4d90e1f4a32172f47bdd
skyfortsec <itjoeldazz@gmail.com>
2026-01-17 13:07:34 +0000

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "git202"
     key            = "trudy-eks/terraform.tfstate"
-    region         = "eu-west-2"
+    region         = "eu-north-1"
     dynamodb_table = "YOUR_TF_LOCK_TABLE"
     encrypt        = true
   }
